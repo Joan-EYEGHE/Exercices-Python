71ed6ea46386b48f6f67732bf693bb1964d18ecb
EYEGHE Joan <joanarcher26@gmail.com>
2026-01-24 22:26:14 +0000

    Correction d'exercices

---
+++ Test.py
@@ -0,0 +1,24 @@
+# Déclarer une variable et afficher son contenu dans le terminal
+
+# 
+# prenom ="Célia"
+# nom= input("Ecris ton nom: ")
+
+
+# Une liste
+# JoursDeLaSemaine=[ "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+# message="Bonjour le boss."
+
+
+# Pour définir une fonction on fait appel au mot clé "def"
+# Tu donnes un nom qui commence par une miniscule
+# tu ajoutes des parenthèses
+# et tu mets le signe deux points (:) après les parenthèses
+
+# Pour le corps d"une fonction contient les instructions (actions) qu'elle doit exécuter (faire)
+
+
+def numeroTelephone():
+    print("Veuillez me contacter au : 77XXXXXXX")
+
+numeroTelephone()
